4997c90e31629ed59e7fc1ad1bdd5c14243d3915
uisproject <uisprojectxyz@gmail.com>
2022-05-17 02:12:06 +0700

    init experiment

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import HookExperiment from "./HookExperiments/HookExperiment";
+import UseEffectProblem from "./UseEffectProblem/UseEffectProblem";
+
+function App() {
+  return (
+    <>
+      {/* Just comment if you want to experiment something else */}
+
+      {/* Experimenting New React18 Hook  */}
+      {/* <HookExperiment /> */}
+
+      {/* useEffect problem */}
+      <UseEffectProblem />
+    </>
+  );
+}
+
+export default App;
